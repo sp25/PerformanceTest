ff4ef91f183adb2a47629eb2df184ec59da55701
sp25 <simon.perron@gmail.com>
2017-12-09 18:26:14 -0500

    inital

--- server.js
+++ server.js
@@ -142,10 +142,10 @@ function init(){
     });
 }
 
-var server = app.listen(process.env.PORT || 8081, function () {
+/*var server = app.listen(process.env.PORT || 8081, function () {
 
     var host = server.address().address;
     var port = server.address().port;
     init();
     console.log("App listening at http://%s:%s", host, port)
-});
+});*/
