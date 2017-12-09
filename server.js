2847033b84085519b09c0b291c7b1e269177a764
sp25 <simon.perron@gmail.com>
2017-12-09 17:31:53 -0500

    inital

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ function init(){
     });
 }
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
 
     var host = server.address().address;
     var port = server.address().port;
