9b59c688d0a79ccd379d9458013d0d72fcee74eb
sp25 <simon.perron@gmail.com>
2017-12-09 22:32:31 -0500

    inital

--- server.js
+++ server.js
@@ -67,8 +67,6 @@ app.get('/performances', function(req, res) {
 
         console.log("Début de la requête sur BD :" + urlHero);
 
-        db.dataset1_2dsp_l.find({ "geometry": {"$near":{"$geometry":positionPoint, $maxDistance:1000}} })
-
         var cursor = db.collection("dataset1_2d_l").find({ "geometry": {"$near":{"$geometry":[ 7.562892923, 51.535703584 ], $maxDistance:1000}} }, {explain: true}).toArray(function (err, explanation) {
         //var cursor = db.collection(donnees).find(query, {explain: true}).toArray(function (err, explanation) {
 
